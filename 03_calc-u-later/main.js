e3dcde37628f123b01052ec58bc60a4b50af8489
Hannes Roever <h.roever@zertificon.com>
2023-09-22 20:25:18 +0200

    - adjust cs class names - change var to constants and let - add feature to also use keyboard buttons for input, including backspace and enter - implement calculation as requested using the basic eval function of js

--- 03_calc-u-later/main.js
+++ 03_calc-u-later/main.js
@@ -1,23 +1,52 @@
-var calcNums = document.querySelectorAll('.numbtn div');
-for (var i = 0; i < calcNums.length; i++) {
-    calcNums[i].addEventListener("click", function(p) { pushToOutput(p.currentTarget.innerText); });
+const calcNums = document.querySelectorAll('.num-button div');
+for (let i = 0; i < calcNums.length; i++) {
+    calcNums[i].addEventListener("click", function(p) { 
+        pushToOutput(p.currentTarget.innerText); 
+    });
 }
 
-var calcOps = document.querySelectorAll('.opsbtns div');
-for (var i = 0; i < calcOps.length; i++) {
-    calcOps[i].addEventListener("click", function(p) { pushToOutput(p.currentTarget.title); });
+const calcOps = document.querySelectorAll('.ops-buttons-container div');
+for (let i = 0; i < calcOps.length; i++) {
+    calcOps[i].addEventListener("click", function(p) { 
+        pushToOutput(p.currentTarget.title); 
+    });
 }
 
-document.getElementById('equals').addEventListener("click", function() { calculate(); });
-document.getElementById('reset').addEventListener("click", function() { reset(); });
+document.getElementById('equals').addEventListener("click", function() { 
+    calculate(); 
+});
+
+document.getElementById('reset').addEventListener("click", function() { 
+    reset(); 
+});
+
+document.addEventListener("keydown", function(event) {
+    const key = event.key;
+    if (/^[0-9+\-*\/]$/.test(key)) {
+        pushToOutput(key.valueOf())
+    } else if (key === "Enter") {
+        calculate()
+    } else if (key === "Backspace") {
+        popFromOutput()
+    }
+});
 
 function pushToOutput(value) {
     document.getElementById("output").innerText += value;
 }
 
+function popFromOutput() {
+    let output = document.getElementById("output");
+    output.innerText = output.innerText.slice(0, -1);
+}
+
 function calculate() {
-    var output = document.getElementById("output");
-    output.innerText = "Not Implemented";
+    const output = document.getElementById("output");
+    try {
+        output.innerText = eval(output.innerText);
+    } catch (error) {
+        output.innerText = "Error";
+    }
 }
 
 function reset() {
