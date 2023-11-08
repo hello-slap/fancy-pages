bb8f0ba646b16b4b67ae0ab4cac5377be81ffcc8
BuDDi <buddizza@web.de>
2023-11-08 14:32:33 +0100

    use immediatialy invoked function expression

--- 03_calc-u-later/main.js
+++ 03_calc-u-later/main.js
@@ -1,4 +1,4 @@
-function initUI() {
+(() => {
     const calcNums = document.querySelectorAll('.numbtn div');
     for (const calcNum of calcNums) {
         calcNum.addEventListener("click", function (p) { pushToOutput(p.currentTarget.innerText); });
@@ -11,17 +11,17 @@ function initUI() {
 
     document.getElementById('equals').addEventListener("click", calculate);
     document.getElementById('reset').addEventListener("click", reset);
-}
 
-function pushToOutput(value) {
-    document.getElementById("output").innerText += value;
-}
+    function pushToOutput(value) {
+        document.getElementById("output").innerText += value;
+    }
 
-function calculate() {
-    const output = document.getElementById("output");
-    output.innerText = eval(output.innerText);
-}
+    function calculate() {
+        const output = document.getElementById("output");
+        output.innerText = eval(output.innerText);
+    }
 
-function reset() {
-    document.getElementById("output").innerText = "";
-}
+    function reset() {
+        document.getElementById("output").innerText = "";
+    }
+})()
